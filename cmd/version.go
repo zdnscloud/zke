f74a2d2178add6970bd6bd3983d2a349393fbd29
hiwyw <wangyanwei@zdns.cn>
2019-05-24 14:31:04 +0800

    update zke config version to v1.0.3

--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,7 @@ import (
 	"github.com/urfave/cli"
 )
 
-const defaultConfigVersion = "v1.0.2"
+const defaultConfigVersion = "v1.0.3"
 
 func VersionCommand() cli.Command {
 	versionFlags := []cli.Flag{
