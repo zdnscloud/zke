c2c8c200e533a8eaadf8e2de5bd7c2b5c02ee836
hiwyw <wangyanwei@zdns.cn>
2019-06-17 14:12:55 +0800

    remove typesnew

--- typesnew/zcloud.go
+++
@@ -1,8 +0,0 @@
-package typesnew
-
-type MonitorConfig struct {
-	// Monitoring server provider
-	MetricsProvider string `yaml:"metrics_provider" json:"metrics_provider,omitempty" norman:"default=metrics-server"`
-	// Metrics server options
-	MetricsOptions map[string]string `yaml:"metrics_options" json:"metrics_options,omitempty"`
-}
