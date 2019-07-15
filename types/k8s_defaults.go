3c25bcd6af839dabe14c789b1930330616cd7dea
yuchunyun <yuchunyun@zdns.cn>
2019-07-15 12:01:41 +0800

    update ClusterAgent and StorageOperator image version

--- types/k8s_defaults.go
+++ types/k8s_defaults.go
@@ -56,11 +56,11 @@ var (
 			IngressBackend:            "zdnscloud/nginx-ingress-controller-defaultbackend:1.4",
 			CoreDNS:                   "zdnscloud/coredns:1.2.6",
 			CoreDNSAutoscaler:         "zdnscloud/cluster-proportional-autoscaler-amd64:1.0.0",
-			ClusterAgent:              "zdnscloud/cluster-agent:v1.8",
+			ClusterAgent:              "zdnscloud/cluster-agent:v1.9",
 			NodeAgent:                 "zdnscloud/node-agent:v1.1",
 			MetricsServer:             "zdnscloud/metrics-server-amd64:v0.3.1",
 			ZKERemover:                "zdnscloud/zke-remove:v0.3",
-			StorageOperator:           "zdnscloud/storage-operator:v0.6",
+			StorageOperator:           "zdnscloud/storage-operator:v0.7",
 		},
 	}
 )
